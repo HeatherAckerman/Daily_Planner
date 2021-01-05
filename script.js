25c6b8e9061beb068a6302b63ab7da4f1f8efae3
HeatherAckerman <hla20115213@gmail.com>
2021-01-05 11:02:45 -0600

    fixed text area color everything is now fully functional

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ $(document).ready(function () {
         $("#currentDay").text(moment().format('LLLL'));
     };
 
-    var hour = moment().hours();
-
     //This function adds a past, present, or future class to each html input. It determines the class by checking the current time against each time row on the schedule
     function textareaColor() {
 
         $("textarea").each(function () {
+
+            var hour = moment().hours();
             // console.log(hour)
             var inputId = $(this).attr("id");
             // console.log(inputId);
             var inputId2 = parseInt(inputId);
-            // console.log(inputId2);
+            console.log(inputId2);
 
             if (inputId2 < hour) {
                 $(this).addClass("past");
@@ -29,30 +29,30 @@ $(document).ready(function () {
 
 
     // This will save the users input to local storage when save is clicked
-    $(".saveBtn").on("click", function() {
+    $(".saveBtn").on("click", function () {
         // get nearby values
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-    
+
         // save in localStorage
         localStorage.setItem(time, value);
-      });     
+    });
 
-  // load any saved data from localStorage
-  $("#6 .description").val(localStorage.getItem("6"));
-  $("#7 .description").val(localStorage.getItem("7"));
-  $("#8 .description").val(localStorage.getItem("8"));
-  $("#9 .description").val(localStorage.getItem("9"));
-  $("#10 .description").val(localStorage.getItem("10"));
-  $("#11 .description").val(localStorage.getItem("11"));
-  $("#12 .description").val(localStorage.getItem("12"));
-  $("#13 .description").val(localStorage.getItem("13"));
-  $("#14 .description").val(localStorage.getItem("14"));
-  $("#15 .description").val(localStorage.getItem("15"));
-  $("#16 .description").val(localStorage.getItem("16"));
-  $("#17 .description").val(localStorage.getItem("17"));
-  $("#18 .description").val(localStorage.getItem("18"));
-   
+    // load any saved data from localStorage
+    $("#6 .description").val(localStorage.getItem("6"));
+    $("#7 .description").val(localStorage.getItem("7"));
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+
     getCurrnetDay();
     textareaColor();
 });
